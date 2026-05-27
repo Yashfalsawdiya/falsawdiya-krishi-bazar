28371349393b911eb7c50af58ae9063d573f641a
Yash Falsawdiya <yashfalsawdiya36@gmail.com>
2026-05-27 11:15:59 +0530

    chore: migrate to .webmanifest and remove sharp dependency

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,6 @@ export default defineConfig(({mode}) => {
       tailwindcss(),
       VitePWA({
         registerType: 'autoUpdate',
-        manifestFilename: 'manifest.json',
         includeAssets: [],
         manifest: {
           name: 'फल्सावदिया कृषि बाज़ार',
